eae677aa94ca8b50da1a657c824068deb74e9020
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-07-11 14:53:15 -0300

    feat: add HTTP error handler function as middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
 import express from "express";
+import "express-async-errors";
+import { errorHandler } from "./middlewares/ErrorHandler/ErrorHandler";
 import "./database";
 import { router } from "./routes";
 
@@ -7,5 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 app.listen(1000, () => console.log("Server is running NLW"));
