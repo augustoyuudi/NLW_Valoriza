769904c3f19f3f2d0d5be22be1eafdf3b44b5ad2
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-07-12 22:56:28 -0300

    feat: add password column to user table

--- src/service/Users/CreateUserService.ts
+++ src/service/Users/CreateUserService.ts
@@ -1,14 +1,16 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from "@repositories/UsersRepositories";
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
   name: string,
   email: string,
-  admin?: boolean
+  password: string,
+  admin?: boolean,
 }
 
 class CreateUserService {
-  async execute({name, email, admin} : IUserRequest) {
+  async execute({name, email, password, admin} : IUserRequest) {
     if (!email) {
       throw new Error("Incorrect email");
     }
@@ -22,9 +24,12 @@ class CreateUserService {
       throw new Error("User already exists");
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
+      password: hashedPassword,
       admin,
     });
 
